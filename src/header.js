49cd9fcec972a80f4f9b4bbdf47d9f3ea3721aff
Vladimir <shikarn0_3000@inbox.ru>
2019-02-04 00:42:44 +0300

    get started

---
+++ src/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class PageHeader extends React.Component {
+  render() {
+    return (
+      <header className='page-header'>
+				<ul>
+					<li className="header-menu"><a href="http://#">1</a></li>
+					<li className='logo'><a href="http://#">Easy<strong>Shape</strong></a></li>
+					<li className='cart'><a href="http://#" alt='Cart'></a></li>
+				</ul>
+      </header>
+    )
+  }
+}
+
+export default PageHeader
---
+++ src/product.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Product extends React.Component {
+    render() {
+        return (
+            <div className='product'>
+                <img className='product-image' src="" alt=""/>
+                <p className="product-title">Christina Aguilera Royal «Black & Red»</p>
+            </div>
+        )
+    }
+  }
+
+  export default Product
