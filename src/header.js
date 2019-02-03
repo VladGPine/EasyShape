b9b06dfc6ec8989b76df20a59b39d2651f16d43f
Vladimir <shikarn0_3000@inbox.ru>
2019-02-04 00:55:04 +0300

    header done

--- src/header.js
+++ src/header.js
@@ -5,7 +5,7 @@ class PageHeader extends React.Component {
     return (
       <header className='page-header'>
 				<ul>
-					<li className="header-menu"><a href="http://#">1</a></li>
+					<li className="header-menu"><a href="http://#" alt='Menu'></a></li>
 					<li className='logo'><a href="http://#">Easy<strong>Shape</strong></a></li>
 					<li className='cart'><a href="http://#" alt='Cart'></a></li>
 				</ul>
