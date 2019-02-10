70c550295156a8cbc68345467f438a9a13bdc652
Vladimir <shikarno_3000@inbox.ru>
2019-02-10 23:31:36 +0300

    one more small fix with grid

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,11 +7,13 @@ class Product extends React.Component {
 	render() {
 		return (
 			<div className='product'>
-				<img className='product-image' src={require('../img/productPhoto.jpg')} alt="" />
-				<p className="product-title">Christina Aguilera Royal «Black & Red»</p>
-				<div className='price'>
-					<span className="discount">2 650</span> 1 990
-				</div>
+				<a href='http://#' alt="">
+					<img className='product-image' src={require('../img/productPhoto.jpg')} alt="" />
+					<p className="product-title">Christina Aguilera Royal «Black & Red»</p>
+					<div className='price'>
+						<span className="discount">2 650</span> 1 990
+					</div>
+				</a>
 			</div>
 		)
 	}
