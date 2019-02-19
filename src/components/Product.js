6ed7b376c64571771cb0f8c7c22200d13f2e4375
Vladimir <shikarno_3000@inbox.ru>
2019-02-19 23:11:00 +0300

    small fix with prices

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,14 +3,35 @@ import React from 'react';
 import '../css/product.css';
 
 
-function Product (props) {
+function Product(props) {
+	
 	return (
 		<div className='product' key={props.product.id}>
 			<a href='/' alt="">
 				<img className='product-image' src={props.product.imgUrl} alt="" />
 				<p className="product-title">{props.product.title}</p>
-				<div className='price'>
-					<span className="discount">{props.product.price}</span> {props.product.discount}
+				<div className='price-block'>
+					<span className="price" 
+						style={{
+							color: props.product.discount ? "#ccd1d9" : "#2e2b40",
+							textDecoration: props.product.discount ? "line-through" : "none"
+						}}>{props.product.price.toLocaleString(
+						'ru-RU', 
+						{
+							style: 'currency', 
+							minimumFractionDigits: 0, 
+							currency: 'RUB'
+						})}
+					</span>
+					<span className="discounted-price" 
+						style={{ opacity: props.product.discount ? 1 : 0 }}> {props.product.discount.toLocaleString(
+							'ru-RU', 
+							{ 
+								style: 'currency', 
+								minimumFractionDigits: 0, 
+								currency: 'RUB' 
+							})}
+					</span>
 				</div>
 			</a>
 		</div>
