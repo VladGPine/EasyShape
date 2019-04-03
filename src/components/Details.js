3b4512e386f804b6667f57b5b396321787c0974f
Vladimir <shikarno_3000@inbox.ru>
2019-04-03 13:17:02 +0300

    products fix

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,13 +4,13 @@ import '../css/details.css';
 
 const Details = props => {
   return (
-    <div className='product'>
-      <img className='product-image' src='../img/1.jpg' alt="" />
-      <p className='category'>Лосины</p>
-      <p className="product-title">My Heart Will Go On</p>
-      <div className='price-block'>
-        <span className="price"></span>
-        <span className="discounted-price"></span>
+    <div className='details'>
+      <img className='details-product-image' src='../img/1.jpg' alt="" />
+      <p className='details-category'>Лосины</p>
+      <p className="details-product-title">My Heart Will Go On</p>
+      <div className='details-price-block'>
+        <span className="details-price"></span>
+        <span className="details-discounted-price"></span>
       </div>
     </div>
   )
