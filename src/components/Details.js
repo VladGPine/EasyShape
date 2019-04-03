2b243ceeb32b2378cd5e2a69fffead4a2f600095
Vladimir <shikarno_3000@inbox.ru>
2019-04-03 13:31:08 +0300

    details UI fix

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,12 +6,15 @@ const Details = props => {
   return (
     <div className='details'>
       <img className='details-product-image' src='../img/1.jpg' alt="" />
-      <p className='details-category'>Лосины</p>
-      <p className="details-product-title">My Heart Will Go On</p>
-      <div className='details-price-block'>
-        <span className="details-price"></span>
-        <span className="details-discounted-price"></span>
+      <div className='description'>
+        <p className='details-category'>Лосины /</p>
+        <p className="details-product-title">My Heart Will Go On</p>
+        <div className='details-price-block'>
+          <span className="details-price"></span>
+          <span className="details-discounted-price"></span>
+        </div>
       </div>
+
     </div>
   )
 }
