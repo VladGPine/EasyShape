98c26ce0f05f078e5592a4ab09b5a1b3bb09537c
Vladimir <shikarno_3000@inbox.ru>
2019-04-11 13:51:21 +0300

    details page UI in the process

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,13 +5,46 @@ import '../css/details.css';
 const Details = props => {
   return (
     <div className='details'>
-      <img className='details-product-image' src='../img/1.jpg' alt="" />
+      <div className='details-slider'>
+        <img className='product-full-image' src='../img/my-heart-will-go-on-gray-pink/my-heart-will-go-on-fullphoto-min.jpg' alt="" />
+        <ul className='preview-images'>
+          <li className='preview-image'>
+            <a href='/'>
+              <img src='../img/my-heart-will-go-on-gray-pink/my-heart-will-go-on-preview-1.jpg' alt='preview' />
+            </a>
+
+          </li>
+          <li className='preview-image'>
+            <a href='/'>
+              <img src='../img/my-heart-will-go-on-gray-pink/my-heart-will-go-on-preview-2.jpg' alt='preview' />
+            </a>
+
+          </li>
+          <li className='preview-image'>
+            <a href='/'>
+              <img src='../img/my-heart-will-go-on-gray-pink/my-heart-will-go-on-preview-3.jpg' alt='preview' />
+            </a>
+
+          </li>
+        </ul>
+      </div>
+
       <div className='description'>
         <p className='details-category'>Лосины /</p>
-        <h2 className="details-product-title">My Heart Will <br/>Go On «Gray—Pink»</h2>
-        <div className='details-price-block'>
-          <span className="details-price">7290</span>
-          <span className="details-discounted-price">2700</span>
+        <h2 className="details-product-title">My Heart Will <br />Go On «Gray—Pink»</h2>
+        <div className='details-price'>
+          <span className="product-price">7290</span>
+          <span className="product-discounted-price">2700</span>
+        </div>
+        <div className='product-description'>
+          <p>Bona Classic  — это годами выверенная модель лосин. Это родоначальник, неповторимый оригинал. Такие должны быть у каждой девушки, и чем больше цветов, тем лучше, ведь каждый цвет уникален, также, как и вы, как и ваше настроение.</p>
+        </div>
+        <div className='product-materials'>
+          <p className='product-material'><strong>Ткань:</strong> бифлекс</p>
+          <p className='product-consist'><strong>Cостав:</strong> полиамид — 50%, полиэстер — 44%, эластан — 6%</p>
+        </div>
+        <div className='sizes'>
+          <ul></ul>
         </div>
       </div>
 
