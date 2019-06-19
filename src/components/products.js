93210d737a710ecec1d350a811c4ef9a308bbf9d
Vladimir <shikarno_3000@inbox.ru>
2019-06-19 16:14:57 +0300

    comments into product.js has been deleted

--- src/components/products.js
+++ src/components/products.js
@@ -1,8 +1,8 @@
 const productsData = [
 	{
 		id: 1,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/1.jpg', // путь к картинке товара
+		category: 'leggins', 
+		imgUrl: '../img/1.jpg',
 		title: 'Bona Corsage «Black»',
 		price: 2200,
 		discount: 0
@@ -10,8 +10,8 @@ const productsData = [
 
 	{
 		id: 2,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/2.jpg', // путь к картинке товара
+		category: 'leggins', 
+		imgUrl: '../img/2.jpg', 
 		title: 'Bona Push Up «Dark Gray» ',
 		price: 2300,
 		discount: 0,
@@ -19,16 +19,16 @@ const productsData = [
 	},
 	{
 		id: 3,
-		category: 'top', //или как они на английском
-		imgUrl: '../img/3.jpg', // путь к картинке товара
+		category: 'top',
+		imgUrl: '../img/3.jpg',
 		title: 'Rashguard «Black» ',
 		price: 1700,
 		discount: 0
 	},
 	{
 		id: 4,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/4.jpg', // путь к картинке товара
+		category: 'leggins',
+		imgUrl: '../img/4.jpg',
 		title: 'DeJaVu «Black»',
 		price: 2900,
 		discount: 0
@@ -36,72 +36,72 @@ const productsData = [
 
 	{
 		id: 5,
-		category: 'kombez', //или как они на английском
-		imgUrl: '../img/5.jpg', // путь к картинке товара
+		category: 'kombez',
+		imgUrl: '../img/5.jpg',
 		title: 'Arcana Sindicate «Black» ',
 		price: 4600,
 		discount: 0
 	},
 	{
 		id: 6,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/6.jpg', // путь к картинке товара
+		category: 'leggins',
+		imgUrl: '../img/6.jpg',
 		title: 'Bona Corsage «Dark Green» ',
 		price: 2900,
 		discount: 0
 	},
 	{
 		id: 7,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/7.jpg', // путь к картинке товара
+		category: 'leggins',
+		imgUrl: '../img/7.jpg',
 		title: 'Bona Mafia Dark Gray ',
 		price: 2900,
 		discount: 0
 	},
 	{
 		id: 9,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/9.jpg', // путь к картинке товара
+		category: 'leggins',
+		imgUrl: '../img/9.jpg',
 		title: 'Bona Mafia «Black» ',
 		price: 2900,
 		discount: 0
 	},
 	{
 		id: 10,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/10.jpg', // путь к картинке товара
+		category: 'leggins',
+		imgUrl: '../img/10.jpg',
 		title: 'Bona Classic «UtiPuti» ',
 		price: 2900,
 		discount: 0
 	},
 	{
 		id: 11,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/11.jpg', // путь к картинке товара
+		category: 'leggins',
+		imgUrl: '../img/11.jpg',
 		title: 'Bona Classic «UtiPuti» ',
 		price: 2900,
 		discount: 2550
 	},
 	{
 		id: 12,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/12.jpg', // путь к картинке товара
+		category: 'leggins',
+		imgUrl: '../img/12.jpg',
 		title: 'Bona Classic «Black» ',
 		price: 1600,
 		discount: 0
 	},
 	{
 		id: 13,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/13.jpg', // путь к картинке товара
+		category: 'leggins',
+		imgUrl: '../img/13.jpg',
 		title: 'Bona Stingray «Dark Gray» ',
 		price: 2900,
 		discount: 0
 	},
 	{
 		id: 14,
-		category: 'leggins', //или как они на английском
-		imgUrl: '../img/14.jpg', // путь к картинке товара
+		category: 'leggins',
+		imgUrl: '../img/14.jpg',
 		title: 'Arcana Sindicate «Cobra» ',
 		price: 4600,
 		discount: 0
